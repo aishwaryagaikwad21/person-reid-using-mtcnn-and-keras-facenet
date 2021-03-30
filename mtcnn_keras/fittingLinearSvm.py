6b12df2ce768a9efea8520c8ef33bf2358a89b4d
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2021-03-31 00:25:26 +0530

    mtcnn_keras

---
+++ mtcnn_keras/fittingLinearSvm.py
@@ -0,0 +1,36 @@
+from numpy import load
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
+from sklearn.svm import SVC
+# load dataset
+data = load('5-celebrity-faces-embeddings.npz')
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+print('Dataset: train=%d, test=%d' % (trainX.shape[0], testX.shape[0]))
+# normalize input vectors
+in_encoder = Normalizer(norm='l2')
+trainX = in_encoder.transform(trainX)
+testX = in_encoder.transform(testX)
+# label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(trainy)
+trainy = out_encoder.transform(trainy)
+testy = out_encoder.transform(testy)
+# fit model
+model = SVC(kernel='linear', probability=True)
+model.fit(trainX, trainy)
+# predict
+yhat_train = model.predict(trainX)
+yhat_test = model.predict(testX)
+# score
+score_train = accuracy_score(trainy, yhat_train)
+score_test = accuracy_score(testy, yhat_test)
+# summarize
+print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
+
+#Running the example first confirms that the number of samples 
+# in the train and test datasets is as we expect
+#Next, the model is evaluated on the train and test dataset, 
+# showing perfect classification accuracy. This is not surprising given 
+# the size of the dataset and the power of the face detection and face 
+# recognition models used.
