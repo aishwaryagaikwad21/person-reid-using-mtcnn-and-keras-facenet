c368c8e31c285cc7aa5f95474a015e743f156b53
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2021-04-04 18:01:51 +0530

    reidentification

---
+++ mtcnn_keras/count.py
@@ -0,0 +1,61 @@
+import os
+import cv2
+from PIL import Image
+from keras.preprocessing import image
+from numpy import asarray
+from mtcnn.mtcnn import MTCNN
+
+image = Image.open('joe.jpg')
+image = image.convert('RGB')
+pixels = asarray(image)
+detector = MTCNN()
+results = detector.detect_faces(pixels)
+x1, y1, width, height = results[0]['box']
+x1, y1 = abs(x1), abs(y1)
+x2, y2 = x1 + width, y1 + height
+	# extract the face
+face = pixels[y1:y2, x1:x2]
+
+#For training set
+files_train = folders_train = 0
+path_train = 'archive/train'
+
+for _, dirnames, filenames in os.walk(path_train):
+  # ^ this idiom means "we won't be using this value"
+    files_train += len(filenames)
+    folders_train += len(dirnames)
+
+directory = os.path.join(path_train,str(folders_train))
+os.mkdir(directory)
+
+count = 0
+while count<11:
+    file_train = 'archive/train/'+ str(folders_train) + '/' + str(count) + '.jpg'
+    image = "joe.jpg"
+    img = cv2.imread(image)
+    print(file_train)
+    cv2.imwrite(file_train, img)
+    count+=1
+    #print(img)
+
+
+#For val set
+files_val = folders_val = 0
+path_val = 'archive/val'
+
+for _, dirnames, filenames in os.walk(path_val):
+  # ^ this idiom means "we won't be using this value"
+    files_val += len(filenames)
+    folders_val += len(dirnames)
+
+directory = os.path.join(path_val,str(folders_val))
+os.mkdir(directory)
+
+count = 0
+while count<6:
+    file_val = 'archive/val/'+ str(folders_val) + '/' + str(count) + '.jpg'
+    image = "joe.jpg"
+    img = cv2.imread(image)
+    print(file_val)
+    cv2.imwrite(file_val, img)
+    count+=1
--- mtcnn_keras/pe_reid.py
+++ mtcnn_keras/pe_reid.py
@@ -5,12 +5,9 @@ from matplotlib import pyplot
 from numpy import savez_compressed
 from numpy import asarray
 from mtcnn.mtcnn import MTCNN
-
 from numpy import expand_dims
 from numpy import load
 from keras.models import load_model
-
-
 from random import choice
 from numpy import load
 from numpy import expand_dims
@@ -18,15 +15,14 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import Normalizer
 from sklearn.svm import SVC
 from matplotlib import pyplot
-
-
+#load model
 model = load_model('facenet_keras.h5')
-
+#load numpy array dataset
 data = load('faces-dataset.npz')
 testX_faces = data['arr_2']
 #image to array
 required_size=(160,160)
-image = Image.open('images/mon.jpg')
+image = Image.open('joe.jpg')
 image = image.convert('RGB')
 pixels = asarray(image)
 detector = MTCNN()
@@ -40,11 +36,6 @@ face = pixels[y1:y2, x1:x2]
 image = Image.fromarray(face)
 image = image.resize(required_size)
 face_array = asarray(image) #imp
-#testX_faces = face_array
-#print(face_array['arr_0'])
-#print(face_array)
-
-
 #Embedding
 face_pixels = face_array.astype('float32')
 	# standardize pixel values across channels (global)
@@ -57,28 +48,8 @@ yhat = model.predict(samples)
 emb = yhat #imp
 in_encoder = Normalizer(norm='l2')
 emb = in_encoder.transform(emb)
-print(emb)
-
-
-
-
-
-
-from random import choice
-from numpy import load
-from numpy import expand_dims
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import Normalizer
-from sklearn.svm import SVC
-from matplotlib import pyplot
-# load faces
-data = load('faces-dataset.npz')
-testX_faces = data['arr_2']
-#print(data['arr_2'])
-#print(testX_faces)
 # load face embeddings
 data = load('faces-embeddings.npz')
-#print(len(data['arr_2']))
 trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
 # normalize input vectors
 in_encoder = Normalizer(norm='l2')
@@ -94,24 +65,21 @@ testy = out_encoder.transform(testy)
 model = SVC(kernel='linear', probability=True)
 model.fit(trainX, trainy)
 # test model on a random example from the test dataset
-
 random_face_pixels = face_array
 random_face_emb = emb[0]
-#print(random_face_emb)
-#random_face_class = testy[selection]
-#random_face_name = out_encoder.inverse_transform([random_face_class])
 # prediction for the face
 samples = expand_dims(random_face_emb, axis=0)
 yhat_class = model.predict(samples)
 yhat_prob = model.predict_proba(samples)
 # get name
-
-print(yhat_class)
+#print(yhat_class)
 class_index = yhat_class[0]
 class_probability = yhat_prob[0,class_index] * 100
 print(class_probability)
 if(class_probability<40):
     print('Face not found')
+    pyplot.imshow(face)
+    pyplot.show()
 else:
     predict_names = out_encoder.inverse_transform(yhat_class)
     print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
